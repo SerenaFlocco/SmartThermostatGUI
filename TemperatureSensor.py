76ad10501266424c3122c7f4d4682d620e82433c
NicolaSabino <nicola.sabino94@gmail.com>
2019-05-10 15:01:13 +0200

    working python sc script

--- TemperatureSensor.py
+++ TemperatureSensor.py
@@ -1,5 +1,23 @@
 import paho.mqtt.client as paho
+import Adafruit_DHT as dht
+import time
 broker = "localhost"
 port = 1883
+temperature = 0
+dht_pin = 4 #pin
+
 
 # publisher callback
+def on_publish(client,userdata,result):
+	print("...publishing")
+	pass
+
+publisher = paho.Client("TemperaturePublisher");
+publisher.on_publish = on_publish
+publisher.connect(broker,port)
+
+while True:
+	#read temp and hum
+	h,t = dht.read_retry(dht.DHT22, dht_pin)
+	publisher.publish("temperature",t)
+	time.sleep(1)
