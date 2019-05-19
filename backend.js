a756d59154ae2dcb671d097a09422ec77189ed49
Nicola <nicola.sabino94@gmail.com>
2019-05-19 17:16:23 +0200

    pi wifi!

--- backend.js
+++ backend.js
@@ -11,7 +11,7 @@ const fs        = require('fs');
 const mqtt      = require('mqtt');
 const exec      = require('child_process').exec;
 const wss       = require('ws').Server;
-const wifi        = require('node-wifi');
+const piWifi    = require('pi-wifi');
 
 const filename    = 'settings.json';
 var mqtt_client   = mqtt.connect('mqtt://localhost');
@@ -52,34 +52,12 @@ app.get('/prog', (req, res) => {
 /* wifi settings*/
 app.get('/wifi', (req, res) => {
 
-  wifi.init({
-    iface : null // network interface, choose a random wifi interface if set to null
-  });
 
-  // Scan networks
-  wifi.scan(function(err, networks) {
-      if (err) {
-          console.log(err);
-      } else {
-          console.log(networks);
-          /*
-          networks = [
-              {
-                ssid: '...',
-                bssid: '...',
-                mac: '...', // equals to bssid (for retrocompatibility)
-                channel: <number>,
-                frequency: <number>, // in MHz
-                signal_level: <number>, // in dB
-                quality: <number>, // same as signal level but in %
-                security: 'WPA WPA2' // format depending on locale for open networks in Windows
-                security_flags: '...' // encryption protocols (format currently depending of the OS)
-                mode: '...' // network mode like Infra (format currently depending of the OS)
-              },
-              ...
-          ];
-          */
-      }
+  piWifi.check('myTestNetwork', function(err, result) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(result);
   });
 
   //res.sendFile(path.join(__dirname,'frontend','wifi_page.html'))
