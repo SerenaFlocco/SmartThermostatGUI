679047121b91929ab8749851f4a00aa21219c556
Nicola <nicola.sabino94@gmail.com>
2019-06-02 11:10:48 +0200

    networks

--- controllers/wifi.js
+++ controllers/wifi.js
@@ -9,36 +9,29 @@ var router = express.Router();
 //router.use('/comments', require('./comments'))
 //router.use('/users', require('./users'))
 
-router.get('/', (req, res) => {
-    // list all the available networks
-    /*piWifi.scan(function(err, networks) {
-      if (err) {
-        return console.error(err.message);
-      }
-      //console.log(networks);
-      //var obj = JSON.parse(networks);
-      if(networks.result == "FAIL-BUSY")
-      res.render('error', {
-                message: "failed to load wifi access points: SERVICE BUSY"
-          });
-      else
-      //load the html page
-      res.render('wifi', {
-      avNetworks: networks
-      });
-    });*/
-    
+router.get('/', (req, res) => {  
     // new library 
-    wifi.scan().then((ssids) => {
+    /*wifi.scan().then((ssids) => {
       console.log(ssids);
+
+
       res.render('wifi', {
         avNetworks: ssids
         });
+
+
     }).catch((error) => {
       console.log(error);
       res.render('error', {
         message: "failed to load wifi access points:" + error
       });
+    });*/
+
+    wifi.getNetworks().then((networks) => {
+      console.log(networks);
+      res.render('wifi', {
+        avNetworks: networks
+        });
     });
 });
 
@@ -51,43 +44,6 @@ router.get('/:ssid', (req, res) => {
 });
 
 router.post('/connect', (req, res) => {
-  /*piWifi.status('wlan0', function(err, status) {
-    if (err) {return console.error(err.message);}
-    if(status.wpa_state == 'COMPLETED'){  // If the system is connected 
-      piWifi.disconnect(function(err) {     // disconnect it
-        if (err) {return console.error(err.message);}
-        console.log('Disconnected from network!');
-        // connect it 
-        piWifi.connect(req.body.ssid, req.body.password, function(err) {
-          if (!err) {
-              res.render('ok', {
-                message: 'Connected to the network ' + ssid
-              });
-	    return
-          }else{
-            res.render('error', {
-              message: "failed to connect on " + req.body.ssid 
-            })
-            return console.error(err.message);
-          }
-        });
-      });
-    }else{//otherwise 
-      piWifi.connect(req.body.ssid, req.body.password, function(err) {
-        if (!err) {
-            res.render('ok', {
-              message: 'Connected to the network ' + ssid
-            });
-        }else{
-          res.render('error', {
-            message: "failed to connect on " + req.body.ssid
-          })
-          return console.error(err.message);
-        }
-      });
-    }
-  });*/
-  
   wifi.connect({ssid: req.body.ssid, psk: req.body.password}).then(() => {
     console.log('Connected to network.');
     res.render('ok', {
