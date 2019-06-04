c92e6a89fc20e473346bc717ad7018d74ee46baf
NicolaSabino <nicola.sabino94@gmail.com>
2019-06-04 15:39:00 +0200

    mac address

--- controllers/home.js
+++ controllers/home.js
@@ -4,6 +4,7 @@ var settings = require('../settings.json');
 var router = express.Router();
 var Wifi = require('rpi-wifi-connection');
 var wifi = new Wifi();
+const mac = require('getmac');
 
 // db
 //router.use('/comments', require('./comments'))
@@ -18,21 +19,31 @@ router.get('/', function(req, res) {
    if(settings.mode == 'prog')
 	mode = 'program';
    if(settings.current_temperature != 0)
-	temperature = settings.current_temperature.toFixed(1) + "°C";
-    wifi.getStatus().then((status) => {
+   temperature = settings.current_temperature.toFixed(1) + "°C";
+   
+   
+   
+   wifi.getStatus().then((status) => {
         console.log(status);
+
+      mac.getMac(function(err, macAddress){
+         if (err)  throw err
+         console.log(macAddress);
+      })
+
+      res.render('index', {
+         temperature: temperature,
+         program: mode,
+         season: settings.season,
+         internet: status.ssid,
+         ipAddress: status.ip_address
+      });
 	
-         res.render('index', {
-	         temperature: temperature,
-            program: mode,
-             season: settings.season,
-            internet: status.ssid,
-            ipAddress: status.ip_address
-         });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+         
+   })
+   .catch((error) => {
+      console.log(error);
+   });
 })
 
 module.exports = router
