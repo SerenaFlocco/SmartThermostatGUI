363767179c72bbe7b8046b97e031a2c9f3f41f03
Nicola <nicola.sabino94@gmail.com>
2019-07-02 21:39:56 +0200

    MAJOR FIX

--- AWSclient/RESTclient.js
+++ AWSclient/RESTclient.js
@@ -27,13 +27,16 @@ function getConfig(token, _function) {
 function postConfig(data,response) {
     const myuri = uri + '/user/PL19-11/devices';
 
+    const configuration = getSettings();
+
     let args = {
-        data: {"device_mac":settings.mac, "nickname": settings.nickname, "configuration":settings},
+        data: {"device_mac":settings.mac, "nickname": settings.nickname, "configuration":configuration},
         headers: { 
             "Content-Type": "application/json",
             "Authorization": "JWT " + data.access_token
         }
     };
+
     client.post(myuri, args,_postConfig);
 }
 
@@ -67,11 +70,12 @@ function _getConfigBiss(data, response){
     res2 = res[1].split('", "device_mac":')
     final = res2[0]
     config = JSON.parse(final);
-    console.log(config);
 
+    const configuration = getSettings();
 
     let configTime_ = parseTimestamp(config.timestamp);
-    let settingsTime_ = parseTimestamp(settings.timestamp);
+    let settingsTime_ = parseTimestamp(configuration.timestamp);
+    console.log(configTime_ + " " + settingsTime_)
 
     //check the passive timpestamp
     if(configTime_ > settingsTime_) {
@@ -83,42 +87,56 @@ function _getConfigBiss(data, response){
          * -new temperature?
          */
         console.log("entrato1")
-        if(settings.current_temperature != config.current_temperature) {
-            settings.current_temperature = config.current_temperature;
+        if(configuration.current_temperature != config.current_temperature) {
+            configuration.current_temperature = config.current_temperature;
             eventemitter.emit('newtemp');
         }
-        if(settings.heating != config.heating && config.season == 'winter') {
-            switch(settings.heating) {
-                case 0: settings.heating = config.heating;
+        if(configuration.heating != config.heating && config.season == 'winter') {
+            console.log(configuration.heating)
+            switch(configuration.heating) {
+                case 0: configuration.heating = config.heating;
                         eventemitter.emit('heatingon');
+                        console.log("hratingon");
                         break;
-                case 1: settings.heating = config.heating;
+                case 1: configuration.heating = config.heating;
                         eventemitter.emit('heatingoff');
+                        console.log("hratingoff");
                         break;
             }
         }
-        if(settings.cooling != config.cooling && config.season == 'summer') {
-            switch(settings.cooling) {
-                case 0: settings.cooling = config.cooling;
+        if(configuration.cooling != config.cooling && config.season == 'summer') {
+            console.log(configuration.cooling)
+            switch(configuration.cooling) {
+                case 0: configuration.cooling = config.cooling;
                         eventemitter.emit('coolingon');
+                        console.log("coolingon");
                         break;
-                case 1: settings.cooling = config.cooling;
+                case 1: configuration.cooling = config.cooling;
                         eventemitter.emit('coolingoff');
+                        console.log("coolingoff");
                         break;
             }
         }
+
+        fs.writeFile(filename, JSON.stringify(config), (err) => {
+            if (err) {
+                console.log('Error writing file', err);
+            } else {
+                console.log('Successfully wrote file');
+            }
+        });
 
         //add the remaining case in which only the passive timestamp is set
     }
 
     let configTime = parseTimestamp(config.lastchange);
-    let settingsTime = parseTimestamp(settings.lastchange);
+    let settingsTime = parseTimestamp(configuration.lastchange);
 
     //check the active timestamp!!!
     if(configTime > settingsTime) {
         console.log("entrato2")
-        settings = config;
-        fs.writeFile(filename, JSON.stringify(settings), (err) => {
+        //settings = config;
+        fs.writeFile(filename, JSON.stringify(config), (err) => {
             if (err) {
                 console.log('Error writing file', err);
             } else {
@@ -139,8 +157,27 @@ function parseTimestamp(timestamp) {
     date.setHours(array[1]);
     date.setMinutes(array[2]);
     date.setSeconds(array[3]);
-    console.log(date);
     return date;
+}
+
+
+function getSettings(){
+    /*fs.readFile(filename, 'utf8', (err, jsonString) => {
+	    if (err) {
+		console.log("Error reading file from disk:", err);
+		return;
+	    }*/
+
+    const jsonString = fs.readFileSync(filename, 'utf8');
+
+    try {
+	//console.log(jsonString);
+	const configuration = JSON.parse(jsonString);
+	return configuration;
+    } catch(err) {
+	console.log('Error parsing JSON string:', err);
+    }
+    //});
 }
 
 module.exports = {
