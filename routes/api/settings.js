474bef480967fee001ebbd5eacdbb270505e0d14
Serena <serena.flocco2@gmail.com>
2019-07-03 17:01:55 +0200

    syncfiles added to all the files

--- routes/api/settings.js
+++ routes/api/settings.js
@@ -2,24 +2,35 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const filename = 'settings.json';
-var settings = require('../../settings.json');
+//var settings = require('../../settings.json');
+const syncfiles = require('../../syncfiles.js');
 const timestamp = require('time-stamp');
 const AWSclient = require('../../AWSclient/RESTclient.js');
 
 //Get All Settings
-router.get('/', (req, res) => res.json(settings.event));
+router.get('/', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.event);
+});
 
 //Get the current temperature
-router.get('/currenttemp', (req, res) => res.json(settings.current_temperature));
+router.get('/currenttemp', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.current_temperature);
+});
 
 //Get the current mode
-router.get('/mode', (req, res) => res.json(settings.mode));
+router.get('/mode', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.mode);
+});
 
 //Modify the current mode
 router.put('/mode', (req, res) => {
     const updated = req.body;
     console.log(updated);
     console.log('Received put request for mode settings');
+    const settings = syncfiles.getSettings(filename);
     settings.mode = updated.mode;
     if(settings.mode == 'off') {
         settings.heating = 0;
@@ -27,13 +38,7 @@ router.put('/mode', (req, res) => {
     }
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
 
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
@@ -43,22 +48,18 @@ router.put('/mode', (req, res) => {
 
 //Get the manual temperature
 router.get('/manualtemp', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
     res.json(settings.last_man_temperature);
 });
 
 //Modify the manual temperature
 router.put('/manualtemp', (req, res) => {
     const updated = req.body;
+    const settings = syncfiles.getSettings(filename);
     settings.temp_to_reach = settings.last_man_temperature = updated.last_man_temperature;
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
 
@@ -66,123 +67,117 @@ router.put('/manualtemp', (req, res) => {
 });
 
 //Get the current season
-router.get('/season', (req, res) => res.json(settings.season));
+router.get('/season', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.season);
+});
 
 //Modify the current season
 router.put('/season', (req, res) => {
     console.log("after");
     const updated = req.body;
+    const settings = syncfiles.getSettings(filename);
     settings.season = updated.season;
     if(settings.season == 'winter')
         settings.cooling = 0;
     else settings.heating = 0;
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(settings.season);
 });
 
 //Get the heating status
-router.get('/heating', (req, res) => res.json(settings.heating));
+router.get('/heating', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.heating);
+});
 
 //Modify the heating status
 router.put('/heating', (req, res) => {
     const updated = req.body;
+    const settings = syncfiles.getSettings(filename);
     settings.heating = updated.heating;
     //SET ONLY THE PASSIVE TIMESTAMP
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(settings.heating);
 });
 
 //Get the cooling status
-router.get('/cooling', (req, res) => res.json(settings.cooling));
+router.get('/cooling', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.cooling);
+});
 
 //Modify the cooling status
 router.put('/cooling', (req, res) => {
     const updated = req.body;
+    const settings = syncfiles.getSettings(filename);
     settings.cooling = updated.cooling;
     //SET ONLY THE PASSIVE TIMESTAMP
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(settings.cooling);
 });
 
 //Get the antifreeze settings
-router.get('/antifreeze', (req, res) => res.json(settings.antifreeze));
+router.get('/antifreeze', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.antifreeze);
+});
 
 //Modify the antifreeze settings
 router.put('/antifreeze', (req, res) => {
     const updated = req.body;
     console.log('Received put request for antifreeze settings');
+    const settings = syncfiles.getSettings(filename);
     settings.antifreeze = updated;
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(settings.antifreeze);
 });
 
 //Get the weekend settings
-router.get('/weekend', (req, res) => res.json(settings.weekend));
+router.get('/weekend', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.weekend);
+});
 
 //Modify the weekend settings
 router.put('/weekend', (req, res) => {
     const updated = req.body;
+    const settings = syncfiles.getSettings(filename);
     settings.weekend = updated;
     console.log('Received put request for weekend mode settings');
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(settings.weekend);
 });
 
 //Get the program settings
-router.get('/prog', (req, res) => res.json(settings.program));
+router.get('/prog', (req, res) => {
+    const settings = syncfiles.getSettings(filename);
+    res.json(settings.program);
+});
 
 //Modify the program settings of a day
 router.put('/prog/:day', (req, res) => {
     const updated = req.body;
     console.log('Received put request for program mode settings');
+    const settings = syncfiles.getSettings(filename);
     let day = req.params.day;
     let to_return;
     switch(day) {
@@ -210,13 +205,7 @@ router.put('/prog/:day', (req, res) => {
     }
     settings.timestamp = timestamp('DD/MM/YYYY:HH:mm:ss');
     settings.lastchange = timestamp('DD/MM/YYYY:HH:mm:ss');
-    fs.writeFile(filename, JSON.stringify(settings), (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote file');
-        }
-    });
+    syncfiles.updateSettings(filename, settings);
     //send post request to configuration
     AWSclient.authenticate(AWSclient.postConfig);
     res.status(201).json(to_return);
